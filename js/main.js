f03dd508d614f4be471cc6fcc81cbdaf89fe1e5a
Atar Mor <atarmor92@gmail.com>
2024-02-13 16:42:27 +0200

    Limit ball size (onBallClick())

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function onBallClick() {
     const elBall = document.querySelector('.ball')
     var ballSize = +elBall.innerText
     ballSize += 50
+    if (ballSize > 400) ballSize = 100
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.style.lineHeight = ballSize + 'px'
