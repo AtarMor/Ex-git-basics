0592266858f00d72e6f14464c56c4b7b0a18e37b
Atar Mor <atarmor92@gmail.com>
2024-02-13 17:46:15 +0200

    Change ball color & update util.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,16 @@
 
 function onBallClick() {
     const elBall = document.querySelector('.ball')
+
     var ballSize = +elBall.innerText
     const addSize = getRandomInt(20, 60)
     ballSize += addSize
     if (ballSize > 400) ballSize = 100
+
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
     elBall.style.lineHeight = ballSize + 'px'
     elBall.innerText = ballSize
+
+    elBall.style.backgroundColor = getRandomColor()
 }
