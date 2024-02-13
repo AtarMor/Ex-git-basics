0d844a848f868108119f956f757026ac629b2bc9
Atar Mor <atarmor92@gmail.com>
2024-02-13 15:07:09 +0200

    Add styling to ball text

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ function onBallClick() {
     const elBall = document.querySelector('.ball')
     var ballSize = +elBall.innerText
     ballSize += 50
-    console.log(ballSize + 'px');
     elBall.style.width = ballSize + 'px'
     elBall.style.height = ballSize + 'px'
+    elBall.style.lineHeight = ballSize + 'px'
     elBall.innerText = ballSize
 }
