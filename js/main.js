67da48de4cb3e29cd6eab7e11d12c1c80c890c0d
Atar Mor <atarmor92@gmail.com>
2024-02-13 20:26:04 +0200

    Add ball4 & onBall4Click()

--- js/main.js
+++ js/main.js
@@ -20,3 +20,22 @@ function onBall3Click() {
     onBallClick(elBall1, 400)
     onBallClick(elBall2, 300)
 }
+
+function onBall4Click() {
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
+    shrinkBall(elBall1, 100)
+    shrinkBall(elBall2, 100)
+}
+
+function shrinkBall(elBall, minDiameter) {
+    var ballSize = +elBall.innerText
+    const reduceSize = getRandomInt(20, 60)
+    ballSize -= reduceSize
+    if (ballSize < minDiameter) return
+
+    elBall.style.width = ballSize + 'px'
+    elBall.style.height = ballSize + 'px'
+    elBall.style.lineHeight = ballSize + 'px'
+    elBall.innerText = ballSize
+}   
